58dc3b0353cf3c45483835e6befa251e655335ea
Jack Dunning <jackdunning23@gmail.com>
2019-03-22 16:43:37 +0000

    add nodemon, express route for image

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
 
-app.listen(PORT, () => console.log('Express JS listening on port ${PORT}.'));
+app.get('/image', function (req, res) {
+    res.send('GET request for /image');
+});
+
+app.listen(PORT, () => console.log(`Express JS listening on port ${PORT}`));
