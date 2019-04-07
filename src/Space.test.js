4d747b8cbfe90efaba10239c18f90dbc784e1075
Jack Dunning <jackdunning23@gmail.com>
2019-04-07 14:07:01 +0100

    remove App contents + replace with Space content

--- src/Space.test.js
+++ src/Space.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Space from './Space';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Space />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
