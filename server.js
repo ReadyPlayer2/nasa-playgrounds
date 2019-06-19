e6b04e91cba0ef09707e8f8219e4ac8c362253c4
Jack Dunning <jackdunning23@gmail.com>
2019-06-19 22:26:39 +0100

    remove temp testing changes from server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!!!')
+    res.send('Hello World!')
 });
 
 app.get('/image', async function (req, res) {
